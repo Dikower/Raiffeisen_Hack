30e1923527f9e9a40bc1bea247d3db7fba66ea29
Leon Minasyan <ierehon1905@gmail.com>
2020-11-14 12:43:35 +0300

    infocard

--- frontend/src/pages/analytics/index.svelte
+++ frontend/src/pages/analytics/index.svelte
@@ -1,8 +1,19 @@
 <script>
   import Chart from "./_components/Chart.svelte";
   import InfoBar from "./_components/InfoBar.svelte";
+  import InfoCard from "./_components/InfoCard.svelte";
 </script>
 
 <!-- routify:options index=3 -->
 <Chart />
-<InfoBar >lel</InfoBar>
+<InfoBar>lel</InfoBar>
+<InfoCard
+  main="Прирост выручки"
+  sub="Посмотрите количество прибыли за год"
+  type="percent"
+  value={75} />
+<InfoCard
+  main="Новые заказы"
+  sub="Посмотрите количество новых заказов за год"
+  type="increase"
+  value={140} />
