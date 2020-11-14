339f506101d769a0b47b232ab9c77b99a9457065
Leon Minasyan <ierehon1905@gmail.com>
2020-11-14 21:28:18 +0300

    fix merge

--- frontend/src/pages/_components/Editor.svelte
+++ frontend/src/pages/_components/Editor.svelte
@@ -1,31 +1,62 @@
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Nunito:ital@1&display=swap" rel="stylesheet">
 <script>
+  let goods = [{'ind': 0, 'id': '', 'emoji': '💡', 'name': '', 'price': '', 'code': '', 'tag': ''}];
+  let api_url = 'https://01d247bffb9a.ngrok.io/';
+  import { getCookie } from "../_api.js";
   let search_text = '';
   import Modal_window from "./Modal_window.svelte";
   let modal = false;
   import Card from "./GoodCard.svelte";
   import Search from "./Search.svelte"
   let chosen_good;
-  let goods = [{'id': 0, 'emoji': '💡', 'title': '', 'price': '', 'art': ''},
-    {'id': 1, 'emoji': '🍕', 'title': 'Пицца', 'price': 123, 'art': '123:32'}, {
-    'id': 2,
-    'emoji': '🍦',
-    'title': 'Мороженное',
-    'price': 50,
-    'art':'123:33'
-  }];
-  function Update_goods(event) {
+  // let goods = [{'ind': 0, 'emoji': '💡', 'name': '', 'price': '', 'code': ''},
+  //   {'ind': 1, 'emoji': '🍕', 'name': 'Пицца', 'price': 123, 'code': '123:32'}, {
+  //   'ind': 2,
+  //   'emoji': '🍦',
+  //   'name': 'Мороженное',
+  //   'price': 50,
+  //   'code':'123:33'
+  // }]
+  async function Update_goods(event) {
     let obj = event.detail.data;
-    if (obj['id'] === 0) {
-      console.log(obj);
-      goods.push({'id': goods.length, 'emoji': obj['emoji'], 'title': obj['title'], 'price': obj['price'], 'art': obj['art']});
+    if (obj['ind'] === 0) {
+      goods.push({'ind': goods.length, 'id': '', 'emoji': obj['emoji'], 'name': obj['name'], 'price': obj['price'], 'code': obj['code'], 'tag': ''});
+      const json_response = await Authorised_fetch_post('positions/create',
+              {'emoji': obj['emoji'], 'name': obj['name'], 'price': Number(obj['price']), 'code': obj['code'], 'tag': ''});
+      console.log(goods);
+      goods[goods.length-1]['id'] = json_response['id'];
+      console.log(goods);
     } else {
-      goods[obj['id']]['emoji'] = obj['emoji'];
-      goods[obj['id']]['title'] = obj['title'];
-      goods[obj['id']]['price'] = obj['price'];
-      goods[obj['id']]['art'] = obj['art'];
+      goods[obj['ind']]['emoji'] = obj['emoji'];
+      goods[obj['ind']]['name'] = obj['name'];
+      goods[obj['ind']]['price'] = obj['price'];
+      goods[obj['ind']]['code'] = obj['code'];
     }
+  }
+  import { onMount } from 'svelte';
+  onMount(async () => {
+    const json_response = await Authorised_fetch_get('positions/all', 'Get');
+    goods = goods.concat(json_response);
+  });
+  async function Authorised_fetch_post(path, obj) {
+   return await fetch(api_url + path, {
+      method: 'Post',
+      headers: new Headers({
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + getCookie('access_token'),
+      }),
+     body: JSON.stringify(obj)
+    }).then(res => res.json());
+  }
+  async function Authorised_fetch_get(path) {
+   return await fetch(api_url + path, {
+      method: 'Get',
+      headers: new Headers({
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + getCookie('access_token'),
+      }),
+    }).then(res => res.json());
   }
 </script>
 
@@ -51,9 +82,9 @@
   <h1>Редактор</h1>
   <Search bind:search_text={search_text}/>
 <div class="goods-container">
-  {#each goods as good}
-    {#if search_text === '' || (search_text.length <= good['title'].length && good['title'].slice(0, search_text.length) === search_text) || good['id']===0 }
-    <Card good={good} on:click="{() => {chosen_good = good; modal = true; }}"/>
+   {#each goods as good}
+    {#if search_text === '' || (search_text.length <= good['name'].length && good['name'].slice(0, search_text.length) === search_text) || good['ind']===0 }
+    <Card good={good} on:click="{() => {chosen_good = good; modal = true}}"/>
       {/if}
   {/each}
   {#if modal}
@@ -61,4 +92,5 @@
   {/if}
 </div>
 </div>
+
 
