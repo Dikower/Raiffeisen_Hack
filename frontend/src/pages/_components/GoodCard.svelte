bef9df05745885bcb1ea333ef4ad697e5b2f86a3
Disimasar <lichko2002@mail.ru>
2020-11-14 15:42:35 +0300

    almost_made_editor

--- frontend/src/pages/_components/GoodCard.svelte
+++ frontend/src/pages/_components/GoodCard.svelte
@@ -1,37 +1,38 @@
 <script>
+  import Emoji from 'svelte-emoji'
   import { createEventDispatcher } from "svelte";
   const dispatch = createEventDispatcher();
   export let thumbnail =
     "https://cdn.shopify.com/s/files/1/2848/4722/products/30f454700daacf066bd7cb2b476eab58_600x.jpg?v=1519595291";
-  export let title = "Pizza";
-  export let price = 123;
-  export let id = 10101010;
+  export let good = {};
   function handleClick() {
-    dispatch("click", id);
+    dispatch("click", good['id']);
   }
 </script>
 
 <style>
   .good-card {
-    width: 173px;
-    height: 173px;
+    width: 170px;
+    height: 170px;
     background-color: #fff;
-    box-shadow: 0px 0px 32px rgba(174, 174, 174, 0.25);
+    box-shadow: 0 0 32px rgba(174, 174, 174, 0.25);
     border-radius: 13px;
     text-align: center;
     font-weight: bold;
     cursor: pointer;
+    margin: 30px;
   }
-  img {
-    border-radius: inherit;
-    padding: 1px;
-    height: 115px;
-    width: 100%;
-    object-fit: cover;
-    display: block;
+  .new_good_svg {
+    height: 60px;
+    width: 60px;
+    margin: 55px;
+  }
+  .emoji_box {
+    font-size: 55px;
+    margin: 10px;
   }
   .title {
-    font-size: 30px;
+    font-size: 20px;
     line-height: 30px;
   }
   .price {
@@ -39,11 +40,17 @@
     font-size: 18px;
   }
 </style>
-
+<svelte:head>
+  <meta charset="UTF-8">
+</svelte:head>
 <div class="good-card" on:click={handleClick}>
-  <img src={thumbnail} alt={title} />
+  {#if good['id'] === 0}
+    <img src="/images/plus.svg" class="new_good_svg" alt="new_good">
+    {:else}
+    <p class="emoji_box">{good['emoji']}</p>
   <div class="info">
-    <div class="title">{title}</div>
-    <div class="price">{price}₽</div>
+    <div class="title">{good['title']}</div>
+    <div class="price">{good['price']}₽</div>
   </div>
+  {/if}
 </div>
---
+++ frontend/src/pages/_components/Search.svelte
@@ -0,0 +1,36 @@
+<script>
+
+</script>
+<div class="component">
+    <button class="search_button"></button>
+    <input type="text" class="search_input">
+</div>
+<style>
+    .component {
+        width: auto;
+        height: 60px;
+        display: flex;
+        background: #FFFFFF;
+        border-radius: 30px;
+        margin: 30px 60px;
+        box-shadow: 0 0 32px rgba(174, 174, 174, 0.25);
+        align-items: center;
+    }
+    .search_input {
+        border: none;
+        outline: none;
+        -webkit-appearance: none;
+        width: 100%;
+        height: 100%;
+        border-radius: 30px;
+        background-color: transparent;
+        margin: 0;
+    }
+    .search_button {
+        height: 26px;
+        width: 30px;
+        border: none;
+        background: transparent url("images/search.svg") no-repeat;
+        margin: 0 0 0 20px;
+    }
+</style>
