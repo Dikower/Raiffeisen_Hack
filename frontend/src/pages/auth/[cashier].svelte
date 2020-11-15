cafa32b16a80b88c7f9b13e0b936b23acf27b1de
Leon Minasyan <ierehon1905@gmail.com>
2020-11-15 10:09:42 +0300

    move analytics

--- frontend/src/pages/auth/[cashier].svelte
+++ frontend/src/pages/auth/[cashier].svelte
@@ -1,12 +1,12 @@
 <script>
-import { params } from '@roxi/routify';
-import { onMount } from 'svelte';
-import { entryCode } from "../goodsStores";
-import { goto } from "@roxi/routify";
+  import { params } from "@roxi/routify";
+  import { onMount } from "svelte";
+  import { entryCode } from "../goodsStores";
+  import { goto } from "@roxi/routify";
 
-onMount(() => {
-    entryCode.set($params['cashier']);
-    localStorage.setItem("entryCode", $params['cashier']);
+  onMount(() => {
+    entryCode.set($params["cashier"]);
+    localStorage.setItem("entryCode", $params["cashier"]);
     $goto("/goods");
-})
+  });
 </script>
