7513e99c4afba945347d4761ad4378fc7ee98f42
Leon Minasyan <ierehon1905@gmail.com>
2020-11-15 10:18:18 +0300

    последнаяя тарнзакция

---
+++ frontend/src/pages/analytics/_components/TransactionCard.svelte
@@ -0,0 +1,29 @@
+<script>
+  import WideCard from "../../_components/WideCard.svelte";
+  export let id;
+  export let time;
+  export let info;
+</script>
+
+<style>
+  .main {
+    font-size: 20px;
+    font-weight: 900;
+  }
+
+  .sub {
+    font-size: 10px;
+    color: #ababab;
+  }
+</style>
+
+<WideCard>
+  <div>
+    <div class="sub">
+      Последняя транзакия
+      {new Date(time).toLocaleDateString()}
+    </div>
+    <div class="main">{info}</div>
+  </div>
+  <div class="sub">ID: {id}</div>
+</WideCard>
