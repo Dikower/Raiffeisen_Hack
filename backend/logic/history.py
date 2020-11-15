5708c39afbbcf1320044cfd2cbc324e6b99af643
Dikower <dmitriy1d01@gmail.com>
2020-11-15 09:45:47 +0300

    Backend is ready

--- backend/logic/history.py
+++ backend/logic/history.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter
-from models import History
+from fastapi import APIRouter, HTTPException
+from models import History, Catalog
 from typing import List
 from tortoise.contrib.pydantic import pydantic_model_creator
 router = APIRouter()
@@ -9,3 +9,14 @@ View = pydantic_model_creator(History)
 @router.get('/', response_model=List[View])
 async def get_history():
     return await View.from_queryset(History.all())
+
+
+@router.get('/{entry_code}/last', response_model=View)
+async def get_last(entry_code: str):
+    catalog = await Catalog.get_or_none(entry_code=entry_code)
+    if not catalog:
+        raise HTTPException(404, 'Catalog not found')
+    filtered = await History.filter(catalog=catalog)
+    if len(filtered) == 0:
+        return {}
+    return await View.from_tortoise_orm(filtered[-1])
